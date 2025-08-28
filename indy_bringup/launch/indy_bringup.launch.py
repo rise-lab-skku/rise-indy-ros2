5acdddaf94251503ea9484b78e117f083550cd6e
jiyou384 <danny384@g.skku.edu>
2025-08-28 22:14:30 +0900

    change Nsquare robot model

--- indy_bringup/launch/indy_bringup.launch.py
+++ indy_bringup/launch/indy_bringup.launch.py
@@ -22,18 +22,24 @@ ROBOT_MODELS = {
     "Indy_RB2": "indy7",
     "Indy_RC1": "indy7",
     "Moby": "indyrp2_v2",
-    "Nsquare": "indyrp2",
+    "Nsquare": "indy7_v2",
 }
 
 ARGUMENTS = [
     DeclareLaunchArgument(
         "robot_name",
         default_value="Moby",
-    )
+    ),
+    DeclareLaunchArgument(
+        "use_fake_hardware",
+        default_value="true",
+        description="Use fake hardware; when true, run joint_state_publisher_gui.",
+    ),
 ]
 
+
 def generate_launch_description():
-    description_path = get_package_share_directory('indy_description')
+    description_path = get_package_share_directory("indy_description")
 
     def add_robot_ip(context, *args, **kwargs):
         robot_ip = ROBOT_IPS.get(
@@ -44,8 +50,13 @@ def generate_launch_description():
                 PythonLaunchDescriptionSource(
                     [description_path, "/launch/visualize_indy.launch.py"]
                 ),
-                launch_arguments={"arm_id": "indy",
-                                  "model": ROBOT_MODELS.get(LaunchConfiguration("robot_name").perform(context))}.items(),
+                launch_arguments={
+                    "arm_id": "indy",
+                    "model": ROBOT_MODELS.get(
+                        LaunchConfiguration("robot_name").perform(context)
+                    ),
+                    "use_fake_hardware": LaunchConfiguration("use_fake_hardware"),
+                }.items(),
             ),
             Node(
                 package="indy_bringup",
@@ -54,25 +65,19 @@ def generate_launch_description():
                 output="screen",
                 parameters=[{"robot_ip": robot_ip}],
                 remappings=[],
-            )]
+            ),
+        ]
 
-    # indy_description = IncludeLaunchDescription(
-    #             PythonLaunchDescriptionSource(
-    #                 [description_path, "/launch/visualize_indy.launch.py"]
-    #             )
-    #         )
-    
-    # joint_state_publisher = Node(
-    #             package="indy_bringup",
-    #             executable="joint_publisher",
-    #             name="joint_publisher",
-    #             output="screen",
-    #             parameters=[],
-    #             remappings=[],
-    #         )
-    
+    joint_state_publisher = Node(
+        package="indy_bringup",
+        executable="joint_state_publisher",
+        name="joint_state_publisher",
+        output="screen",
+        parameters=[],
+        remappings=[],
+    )
+
     ld = LaunchDescription(ARGUMENTS)
-    # ld.add_action(indy_description)
-    # ld.add_action(joint_state_publisher)
+    ld.add_action(joint_state_publisher)
     ld.add_action(OpaqueFunction(function=add_robot_ip))
     return ld
